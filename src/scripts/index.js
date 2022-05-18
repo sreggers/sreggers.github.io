26b28d6a8825cc628251fb8a2c951cd47a030e18
sreggers <sebastiaan@reggers.org>
2022-05-18 22:11:18 +0200

    Implement layout, i18n and scrollspy

---
+++ src/scripts/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+import { createApp } from 'petite-vue';
+import { createI18n } from 'petite-vue-i18n-lite';
+
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        en: {
+            navbar: {
+                about: 'About',
+                experience: 'Experience',
+                education: 'Education',
+                skills: 'Skills',
+            },
+            about: {
+                header: 'Hello there!',
+                text: 'My name is Sebastiaan Reggers and I\'m currently working as a fullstack developer for the inSign software from IS2. As a programmer I get the most satisfaction out of developing an intuative and accessible frontend built on top of a robust and secure backend.',
+            },
+            experience: {
+                header: 'Experience',
+            },
+            education: {
+                header: 'Education',
+            },
+            skills: {
+                header: 'Skills',
+            },
+        },
+        nl: {
+            navbar: {
+                about: 'Introductie',
+                experience: 'Ervaring',
+                education: 'Opleiding',
+                skills: 'Vaardigheden',
+            },
+        },
+    }
+});
+
+createApp(i18n).mount();
+
+window.onload = function(){
+    const scrollspy_elems = document.querySelectorAll('#scrollspy .nav-elem');
+    const info_sections = document.querySelectorAll('.info-section section');
+
+    window.onscroll = () => {
+        let lastIndex = 0;
+        info_sections.forEach((s,i) => {
+            const sectionTopPos = s.getBoundingClientRect().top;
+            if (sectionTopPos < 100) {
+                lastIndex = i;
+            }
+        });
+        scrollspy_elems.forEach(e => e.classList.remove('active'))
+        scrollspy_elems.item(lastIndex).classList.add('active');
+    }
+};
